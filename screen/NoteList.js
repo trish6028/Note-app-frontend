07d73a28ac20416ca5d7344113f40a31e7c65c19
trish6028 <trishanpriyasachin@gmail.com>
2023-08-08 21:15:19 +0530

    create note list page

---
+++ screen/NoteList.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function NoteList() {
+  return (
+    <View>
+      <Text>NoteList</Text>
+    </View>
+  )
+}
