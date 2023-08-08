bd2b5fed84b933ab8d28c6d759fb313872a8db50
trish6028 <trishanpriyasachin@gmail.com>
2023-08-08 18:42:10 +0530

    login Page start

--- screen/Login.js
+++ screen/Login.js
@@ -3,8 +3,11 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <KeyboardAvoidingView
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    style={styles.container}> 
+    
+    
+    </KeyboardAvoidingView>
   )
 }
